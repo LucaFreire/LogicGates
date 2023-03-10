01e036e6b46ed12b65b9428ef40e051a9e022748
Lucas Freire <104910407+LucaFreire@users.noreply.github.com>
2023-03-10 11:16:07 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿bool[] valores = new bool[]{true , false};
+
+GateAND and = new GateAND(valores);
+
+Console.WriteLine(and.FindValue());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public abstract class Components
+{
+    // public bool value { get; set; } = false;
+    protected bool[] valores { get; set; }
+    protected int index { get; set; } = 0;
+    public Components(bool[] values)
+    {
+        for (int i = 0; i < values.Count(); i++)
+        {    
+            this.valores[index] =  values[index];
+            this.index++;
+        }
+    }
+    
+    // public virtual void TurnON() => this.value = true;
+    public virtual bool FindValue() => false;
+}
+
+public class GateAND : Components 
+{
+    public GateAND(bool[] values) : base(values) { }
+    public override bool FindValue() =>  !valores.Take(index).Any(x => !x);
+
+}
+
+public class GateNOT : Components 
+{
+    // TODO: only accept one value to GateNOT
+    public GateNOT(bool[] values) : base(values) { }
+    public override bool FindValue() =>  !valores[0];
+
+}
+
+public class GateOR : Components 
+{
+    public GateOR(bool[] values) : base(values) { }
+    public override bool FindValue() => !valores.Take(index).Any(x => x);
+
+}
